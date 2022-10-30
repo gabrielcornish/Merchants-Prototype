2ac08623381c064e9077c2be0ad41dfc2cf44db0
cubnoble <cub.noble@yahoo.fr>
2022-10-29 21:13:47 -0500

    Merchant Token moving on key press 'C'

---
+++ Player/MerchantToken.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+onready var token = $merchantToken
+onready var destination = $Position3D
+
+var new_transform = Vector3(2,0,0)
+
+var button_pressed = false
+
+func _input(ev):
+	if ev is InputEventKey and ev.scancode == KEY_C:
+		if button_pressed == false:
+			token.translation = destination.global_transform.origin
+			destination.translation += new_transform
+			button_pressed = true
+	else:
+		button_pressed = false	
